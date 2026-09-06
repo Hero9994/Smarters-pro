663847e071e51d5fdc19a467efbe3e926714e8d0
Hero9994 <frra2adnan@gmail.com>
2026-09-06 20:56:16 +0200

    test(alpha): cover adaptive scanner white balance

--- app/src/test/java/app/masahati/mobile/DocumentImageEnhancerDecisionTest.kt
+++ app/src/test/java/app/masahati/mobile/DocumentImageEnhancerDecisionTest.kt
@@ -15,4 +15,39 @@ class DocumentImageEnhancerDecisionTest {
     fun worseCorrectionIsRejected() {
         assertFalse(DocumentImageEnhancer.preferCorrected(74f, 69f))
     }
+
+    @Test
+    fun whiteBalanceCorrectsMildPaperColorCast() {
+        val gains = DocumentImageEnhancer.paperWhiteBalanceGains(
+            averageRed = 232f,
+            averageGreen = 218f,
+            averageBlue = 199f,
+            brightFraction = 0.72f
+        )
+        assertTrue(gains != null)
+        assertTrue(gains!!.third > gains.first)
+    }
+
+    @Test
+    fun whiteBalanceDoesNotTouchAlreadyNeutralPaper() {
+        val gains = DocumentImageEnhancer.paperWhiteBalanceGains(
+            averageRed = 230f,
+            averageGreen = 229f,
+            averageBlue = 228f,
+            brightFraction = 0.74f
+        )
+        assertTrue(gains == null)
+    }
+
+    @Test
+    fun whiteBalanceDoesNotTouchMostlyNonPaperImage() {
+        val gains = DocumentImageEnhancer.paperWhiteBalanceGains(
+            averageRed = 220f,
+            averageGreen = 205f,
+            averageBlue = 190f,
+            brightFraction = 0.18f
+        )
+        assertTrue(gains == null)
+    }
+
 }
