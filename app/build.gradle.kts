5c20f184ea2fc50b4840b616cf2d4258484c8942
Hero9994 <frra2adnan@gmail.com>
2026-09-07 14:54:31 +0200

    feat(alpha): add OpenCV document rectification engine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     }
     implementation("androidx.metrics:metrics-performance:1.0.0")
     implementation("com.github.anrwatchdog:anrwatchdog:1.4.0")
+    implementation("org.opencv:opencv:4.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
